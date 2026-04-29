2f691b189bf77b15bf8d6d36fd95d190003e71fd
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-04-29 13:22:39 -0300

    Update main.tsx

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>CoinHat DEX 🚀</h1>
+      <p>Vite + React funcionando</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
