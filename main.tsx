f433458ee2f510441b1e44a17b6766e79c5952f7
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-04-29 13:28:13 -0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,1 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import SwapPage from "./SwapPage";
 
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SwapPage />
+  </React.StrictMode>
+);
